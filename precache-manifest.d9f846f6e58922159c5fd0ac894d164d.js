99408e1227c934561bfce149efe1e6271a0e3dca
grapedge <grapedge@gmail.com>
2019-10-12 10:01:28 +0800

    update copy function

---
+++ precache-manifest.d9f846f6e58922159c5fd0ac894d164d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1232a622318aa00d101b8479cd8c1309",
+    "url": "/index.html"
+  },
+  {
+    "revision": "38e6c5241270d188e050",
+    "url": "/static/css/main.b4dbfc60.chunk.css"
+  },
+  {
+    "revision": "d4365fc84b6aad3621b0",
+    "url": "/static/js/2.2282442f.chunk.js"
+  },
+  {
+    "revision": "38e6c5241270d188e050",
+    "url": "/static/js/main.02b783aa.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
